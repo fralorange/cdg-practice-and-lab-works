d6a4583005ab922451b45ecbb7c970d72ed0c1e1
fralorange <fralorange@mail.ru>
2023-02-19 17:27:22 +0300

    Complete Lab4 2/3

---
+++ labs/LR4_Rodionov_IVTb-21-2-o/lab4_2.rb
@@ -0,0 +1,24 @@
+require_relative "lab4_1"
+
+MAIN_NAME_PATH = "people.txt"
+RESULTS_NAME_PATH = "results.txt"
+
+age = 0
+full = true
+
+while ((age.to_i != -1) && full)
+  print "Введите своё возраст: "
+  age = gets.chomp
+
+  peopleOfSameAge = where(MAIN_NAME_PATH, age)
+  next unless (File.exists?(RESULTS_NAME_PATH) && (File.readlines(RESULTS_NAME_PATH) & peopleOfSameAge)).empty?
+
+  File.open(RESULTS_NAME_PATH, "a+") do |file|
+    file.write(peopleOfSameAge.join)
+  end
+
+  full = false if (File.readlines(RESULTS_NAME_PATH).sort == File.readlines(MAIN_NAME_PATH).sort)
+end
+
+puts "Результаты: "
+index(RESULTS_NAME_PATH)
--- labs/LR4_Rodionov_IVTb-21-2-o/main.rb
+++
@@ -1,7 +0,0 @@
-require_relative "extensions"
-
-FILE_NAME_PATH = "people.txt"
-
-index(FILE_NAME_PATH)
-puts
-find(FILE_NAME_PATH, 3)
